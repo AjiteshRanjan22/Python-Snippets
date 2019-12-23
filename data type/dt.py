504dd8c2231f11b5b9a25accbc49a9a2788d2727
AjiteshRanjan22 <ajiteshranjan22@gmail.com>
2019-12-23 15:54:56 +0530

    Simple operations

---
+++ data type/dt.py
@@ -0,0 +1,15 @@
+'''Data types and concatenation'''
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+a = int(input())
+b = float(input())
+c = input()
+
+# Print the sum of both integer variables on a new line.
+print(i+a)
+print(d+b)
+print(s+c)
